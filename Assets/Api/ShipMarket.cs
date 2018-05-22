87a5d1cbd6c5c15bf936a5ad9ee4b5ed8b0407e1
Hamour <mickael.hamour@gmail.com>
2018-05-22 11:59:36 +0200

    Playground Manager.prefab

---
+++ Assets/Api/ShipMarket.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipMarket : Market {
+
+    public Dropdown Drop;
+    public ScrollManager ScrollManager;
+
+   // public ScrollRect ScrollRect;
+
+
+    public override void Initialisation()
+    {
+        WebServices.GetItemByCategory(6, OnSucess);
+
+        Drop.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(Drop);
+        });
+
+       // ScrollRect.onValueChanged.AddListener(ScrollManagerValueChanged);
+        
+
+
+    }
+
+    void OnSucess(List<VoGroup> ListItem)
+    {
+
+        List<string> ListDrop = new List<string>();
+        foreach (VoGroup Group in ListItem)
+        {
+            ListDrop.Add(Group.label);
+
+            /*   Debug.Log("OnSucess" + ListItem.Count + "---" + AManager.instance.ListVoItem.Count);
+               foreach (VoItem item in Group.ListItem)
+               {
+                   StartCoroutine(setImage(item));
+               }*/
+
+
+        }
+
+        Drop.AddOptions(ListDrop);
+
+        ScrollManager.GenerateScroll(AManager.instance.ListItem[0].ListItem);
+
+    }
+
+
+    void DropdownValueChanged(Dropdown change)
+    {
+        ScrollManager.GenerateScroll(AManager.instance.ListItem[change.value].ListItem);
+        Debug.Log("New Value : " + AManager.instance.ListItem[change.value].label);
+    }
+
+    
+}
--- Assets/CallWS.cs
+++ Assets/CallWS.cs
@@ -6,61 +6,22 @@ using UnityEngine.UI;
 public class CallWS : MonoBehaviour {
 
 
-    public GameObject parent;
-    public Dropdown Drop;
+    public ShipMarket ShipMarket;
+    
 	// Use this for initialization
 	void Start () {
 
-        WebServices.GetItemByCategory(6, OnSucess);
-
-
-        Drop.onValueChanged.AddListener(delegate {
-            DropdownValueChanged(Drop);
-        });
+        ShipMarket.Initialisation();
 
 
     }
 
 
-    public ScrollManager ScrollManager;
+    
 
-    void DropdownValueChanged(Dropdown change)
-    {
+    
 
-        /* foreach (VoItem item in Group.ListItem)
-         {
-             StartCoroutine(setImage(item));
-         }*/
-
-        Debug.Log("New Value : " + AManager.instance.ListItem[change.value].label);
-        Debug.Log("New Value : " + AManager.instance.ListItem[change.value].ListItem.Count);
-
-
-        ScrollManager.GenerateScroll(AManager.instance.ListItem[change.value].ListItem);
-        Debug.Log("New Value : " + AManager.instance.ListItem[change.value].label) ;
-    }
-
-    void OnSucess(List<VoGroup> ListItem)
-    {
-
-        List<string> ListDrop = new List<string>();
-        foreach (VoGroup Group in ListItem)
-        {
-            ListDrop.Add(Group.label);
-
-         /*   Debug.Log("OnSucess" + ListItem.Count + "---" + AManager.instance.ListVoItem.Count);
-            foreach (VoItem item in Group.ListItem)
-            {
-                StartCoroutine(setImage(item));
-            }*/
-
-
-        }
-
-        Drop.AddOptions(ListDrop);
-
-        
-    }
+    
 
    
 
